4c000c50a34b32d4c9c1ab04c1c3e3f7121c56f3
Rafał Bujok <rafal.bujok@gmail.com>
2020-12-07 10:55:30 +0100

    remove ngDoCheck, add navLinks to subscription

--- src/app/components/content/category-display/category-display.component.ts
+++ src/app/components/content/category-display/category-display.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../../../services/product.service';
 import { NavServiceService } from './../../../services/nav-service.service';
 import { Component, DoCheck, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
@@ -7,18 +8,46 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './category-display.component.html',
   styleUrls: ['./category-display.component.scss']
 })
-export class CategoryDisplayComponent implements OnInit, DoCheck {
-
-  constructor(private navServ: NavServiceService, private route: ActivatedRoute, private location: Location) { }
+export class CategoryDisplayComponent implements OnInit {
+  productList: any;
+  category: string;
+  sub: any;
+  constructor(private navServ: NavServiceService, private route: ActivatedRoute, private productService: ProductService) { }
   navigation: any;
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.category = params.categoryName;
+      console.log(this.category)
+      this.navServ.navlink.map(v => {
+        if (v[1] ===  this.category) {
+          this.navigation = v[2];
+          console.log(v[2])
+        };
+      })
+
+
+      this.productService.getProductDetailsList('product/' + this.category);
+      this.getProductList();
+    }), (err) => console.error(err);
   }
-  ngDoCheck() {
-    const categoryName = this.route.snapshot.paramMap.get('categoryName')
-    this.navServ.navlink.map(v => {
-      if (v[1] === categoryName) {
-        this.navigation = v[2];
-      };
-    })
+  // ngDoCheck() {
+  //   const categoryName = this.route.snapshot.paramMap.get('categoryName')
+  //   this.navServ.navlink.map(v => {
+  //     if (v[1] === categoryName) {
+  //       this.navigation = v[2];
+  //       console.log(v[2])
+  //     };
+  //   })
+  // }
+  getProductList() {
+    this.productService.productDetailList.snapshotChanges().subscribe(list => {
+      this.productList = list.map(item => {
+        return ({  ...item.payload.val() })
+      });
+    });
+
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
