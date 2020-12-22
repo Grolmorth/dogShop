ea0f629da665025ea6f575ec6aab978e24c0f503
Rafał Bujok <rafal.bujok@gmail.com>
2020-12-22 14:06:36 +0100

    fix currencyPipe

--- src/app/pipes/currency-pln.pipe.ts
+++ src/app/pipes/currency-pln.pipe.ts
@@ -1,3 +1,4 @@
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -7,7 +8,7 @@ export class CurrencyPlnPipe implements PipeTransform {
 
   transform(value: number): string {
     const currencyCode: string = 'PLN';
-    let newValue: string = value.toString() +  currencyCode;
+    let newValue: string = value.toFixed(2) + currencyCode;
     return (newValue)
   }
 
